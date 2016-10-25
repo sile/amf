cc5ea428a2f12089387304b217843243d8bc3c4b
Takeru Ohta <phjgt308@gmail.com>
2016-10-26 02:13:14 +0900

    Support references in AMF0 decoding

--- src/amf0.rs
+++ src/amf0.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::rc::Rc;
 use byteorder::BigEndian;
 use byteorder::ReadBytesExt;
 
@@ -10,13 +11,45 @@ pub enum Value {
     Str(Vec<u8>),
     Xml(Vec<u8>),
     Number(f64),
-    Object(Vec<(Vec<u8>, Value)>),
-    Array(Vec<Value>),
-    EcmaArray(Vec<(Vec<u8>, Value)>),
-    TypedObject(TypedObject),
+    Object(Rc<Object>),
+    Array(Rc<Array>),
+    EcmaArray(Rc<EcmaArray>),
+    TypedObject(Rc<TypedObject>),
 
     // TODO: remove
     ObjectEnd,
+}
+#[derive(Debug, Clone, PartialEq)]
+pub enum Sharable {
+    Object(Rc<Object>),
+    Array(Rc<Array>),
+    EcmaArray(Rc<EcmaArray>),
+    TypedObject(Rc<TypedObject>),
+}
+impl Sharable {
+    fn to_value(&self) -> Value {
+        match *self {
+            Sharable::Object(ref x) => Value::Object(x.clone()),
+            Sharable::Array(ref x) => Value::Array(x.clone()),
+            Sharable::EcmaArray(ref x) => Value::EcmaArray(x.clone()),
+            Sharable::TypedObject(ref x) => Value::TypedObject(x.clone()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Array {
+    values: Vec<Value>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct EcmaArray {
+    pub members: Vec<(Vec<u8>, Value)>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Object {
+    pub members: Vec<(Vec<u8>, Value)>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -47,17 +80,26 @@ const MARKER_AVMPLUS_OBJECT: u8 = 0x11;
 #[derive(Debug)]
 pub struct Decoder<R> {
     inner: R,
+    sharables: Vec<Option<Sharable>>,
 }
 impl<R> Decoder<R>
     where R: io::Read
 {
     pub fn new(inner: R) -> Self {
-        Decoder { inner: inner }
+        Decoder {
+            inner: inner,
+            sharables: Vec::new(),
+        }
     }
     pub fn into_inner(self) -> R {
         self.inner
     }
     pub fn decode(&mut self) -> io::Result<Value> {
+        let result = self.decode_value();
+        self.sharables.clear();
+        result
+    }
+    fn decode_value(&mut self) -> io::Result<Value> {
         let marker = try!(self.inner.read_u8());
         match marker {
             MARKER_NUMBER => self.decode_number(),
@@ -67,7 +109,7 @@ impl<R> Decoder<R>
             MARKER_MOVIECLIP => unimplemented!(),
             MARKER_NULL => Ok(Value::Null),
             MARKER_UNDEFINED => Ok(Value::Undefined),
-            MARKER_REFERENCE => unimplemented!(),
+            MARKER_REFERENCE => self.decode_reference(),
             MARKER_ECMA_ARRAY => self.decode_ecma_array(),
             MARKER_OBJECT_END_MARKER => Ok(Value::ObjectEnd),
             MARKER_STRICT_ARRAY => self.decode_strict_array(),
@@ -111,31 +153,56 @@ impl<R> Decoder<R>
         Ok(Value::Xml(buf))
     }
     fn decode_object(&mut self) -> io::Result<Value> {
+        let ref_index = self.sharables.len();
+        self.sharables.push(None);
+
         let pairs = try!(self.decode_pairs());
-        Ok(Value::Object(pairs))
+        let v = Rc::new(Object { members: pairs });
+        self.sharables[ref_index] = Some(Sharable::Object(v.clone()));
+        Ok(Value::Object(v))
     }
     fn decode_typed_object(&mut self) -> io::Result<Value> {
+        let ref_index = self.sharables.len();
+        self.sharables.push(None);
+
         let len = try!(self.inner.read_u16::<BigEndian>()) as usize;
         let mut type_name = vec![0; len];
         try!(self.inner.read_exact(&mut type_name));
         let pairs = try!(self.decode_pairs());
-        Ok(Value::TypedObject(TypedObject {
+        let v = Rc::new(TypedObject {
             type_name: type_name,
             members: pairs,
-        }))
+        });
+        self.sharables[ref_index] = Some(Sharable::TypedObject(v.clone()));
+        Ok(Value::TypedObject(v))
     }
     fn decode_ecma_array(&mut self) -> io::Result<Value> {
+        let ref_index = self.sharables.len();
+        self.sharables.push(None);
+
         let _count = try!(self.inner.read_u32::<BigEndian>());
         let pairs = try!(self.decode_pairs());
-        Ok(Value::EcmaArray(pairs))
+        let v = Rc::new(EcmaArray { members: pairs });
+        self.sharables[ref_index] = Some(Sharable::EcmaArray(v.clone()));
+        Ok(Value::EcmaArray(v))
     }
     fn decode_strict_array(&mut self) -> io::Result<Value> {
+        let ref_index = self.sharables.len();
+        self.sharables.push(None);
+
         let count = try!(self.inner.read_u32::<BigEndian>()) as usize;
         let mut v = Vec::with_capacity(count);
         for _ in 0..count {
-            v.push(try!(self.decode()));
+            v.push(try!(self.decode_value()));
         }
+        let v = Rc::new(Array { values: v });
+        self.sharables[ref_index] = Some(Sharable::Array(v.clone()));
         Ok(Value::Array(v))
+    }
+    fn decode_reference(&mut self) -> io::Result<Value> {
+        let index = try!(self.inner.read_u16::<BigEndian>()) as usize;
+        // TODO: error handlings
+        Ok(self.sharables[index].as_ref().unwrap().to_value())
     }
     fn decode_pairs(&mut self) -> io::Result<Vec<(Vec<u8>, Value)>> {
         let mut pairs = Vec::new();
@@ -143,7 +210,7 @@ impl<R> Decoder<R>
             let len = try!(self.inner.read_u16::<BigEndian>()) as usize;
             let mut key = vec![0; len];
             try!(self.inner.read_exact(&mut key));
-            let value = try!(self.decode());
+            let value = try!(self.decode_value());
             if value == Value::ObjectEnd {
                 break;
             }
@@ -160,6 +227,7 @@ pub fn decode_bytes(mut bytes: &[u8]) -> io::Result<Value> {
 
 #[cfg(test)]
 mod test {
+    use std::rc::Rc;
     use super::*;
 
     #[test]
@@ -206,49 +274,63 @@ mod test {
     #[test]
     fn decodes_object() {
         let input = include_bytes!("testdata/amf0-object.bin");
+        let expected = [(vec![], Value::Str(vec![])),
+                        (to_vec(b"foo"), Value::Str(to_vec(b"baz"))),
+                        (to_vec(b"bar"), Value::Number(3.14))];
         assert_eq!(decode_bytes(&input[..]).unwrap(),
-                   Value::Object([(vec![], Value::Str(vec![])),
-                                  (to_vec(b"foo"), Value::Str(to_vec(b"baz"))),
-                                  (to_vec(b"bar"), Value::Number(3.14))]
-                       .iter()
-                       .cloned()
-                       .collect()));
+                   Value::Object(Rc::new(Object { members: Vec::from(&expected[..]) })));
     }
     #[test]
     fn decodes_typed_object() {
         let input = include_bytes!("testdata/amf0-typed-object.bin");
+        let expected = [(to_vec(b"foo"), Value::Str(to_vec(b"bar"))),
+                        (to_vec(b"baz"), Value::Null)];
         assert_eq!(decode_bytes(&input[..]).unwrap(),
-                   Value::TypedObject(TypedObject {
+                   Value::TypedObject(Rc::new(TypedObject {
                        type_name: to_vec(b"org.amf.ASClass"),
-                       members:
-                           Vec::from(&[(to_vec(b"foo"),
-                                        Value::Str(to_vec(b"bar"))),
-                                       (to_vec(b"baz"),
-                                        Value::Null)][..]),
-                   }));
+                       members: Vec::from(&expected[..]),
+                   })));
     }
     #[test]
     fn decodes_ecma_array() {
         let input = include_bytes!("testdata/amf0-ecma-ordinal-array.bin");
+        let expected = [(to_vec(b"0"), Value::Str(to_vec(b"a"))),
+                        (to_vec(b"1"), Value::Str(to_vec(b"b"))),
+                        (to_vec(b"2"), Value::Str(to_vec(b"c"))),
+                        (to_vec(b"3"), Value::Str(to_vec(b"d")))];
         assert_eq!(decode_bytes(&input[..]).unwrap(),
-                   Value::EcmaArray([(to_vec(b"0"), Value::Str(to_vec(b"a"))),
-                                     (to_vec(b"1"), Value::Str(to_vec(b"b"))),
-                                     (to_vec(b"2"), Value::Str(to_vec(b"c"))),
-                                     (to_vec(b"3"), Value::Str(to_vec(b"d")))]
-                       .iter()
-                       .cloned()
-                       .collect()));
+                   Value::EcmaArray(Rc::new(EcmaArray { members: Vec::from(&expected[..]) })));
     }
     #[test]
     fn decodes_strict_array() {
         let input = include_bytes!("testdata/amf0-strict-array.bin");
+        let expected = [Value::Number(1.0), Value::Str(to_vec(b"2")), Value::Number(3.0)];
         assert_eq!(decode_bytes(&input[..]).unwrap(),
-                   Value::Array(Vec::from(&[Value::Number(1.0),
-                                            Value::Str(to_vec(b"2")),
-                                            Value::Number(3.0)][..])));
+                   Value::Array(Rc::new(Array { values: Vec::from(&expected[..]) })));
     }
+    #[test]
+    fn decodes_reference() {
+        let input = include_bytes!("testdata/amf0-ref-test.bin");
 
+        let object = obj(&[(key("foo"), string("baz")), (key("bar"), number(3.14))][..]);
+        let expected =
+            obj(&[(key("0"), Value::Object(object.clone())),
+                  (key("1"), Value::Object(object.clone()))][..]);
+        assert_eq!(decode_bytes(&input[..]).unwrap(), Value::Object(expected));
+    }
+    fn obj(members: &[(Vec<u8>, Value)]) -> Rc<Object> {
+        Rc::new(Object { members: Vec::from(members) })
+    }
     fn to_vec(bytes: &[u8]) -> Vec<u8> {
         Vec::from(bytes)
+    }
+    fn string(s: &str) -> Value {
+        Value::Str(key(s))
+    }
+    fn number(n: f64) -> Value {
+        Value::Number(n)
+    }
+    fn key(s: &str) -> Vec<u8> {
+        s.as_bytes().iter().cloned().collect()
     }
 }
